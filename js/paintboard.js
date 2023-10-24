52c13b30dcb831dddd8faf6d7192af3bbfa06dc5
Seungmin Song <142411910+xngalsdev@users.noreply.github.com>
2023-10-24 12:08:18 +0900

    그림판 선 그리기, 색상 변경 추가

--- js/paintboard.js
+++ js/paintboard.js
@@ -1,26 +1,81 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+/* 랜덤 색상
+const colors = [
+    "red","orange","yellow","green","blue","purple","black"
+]*/
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+let isPainting = false;
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+
 
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 
-/**
- * 집 그리기
- */
+function onMouseMove(event){
+    if (isPainting){
+        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX,event.offsetY);
+}
 
-// 굵기
-ctx.lineWidth = 10;
+function onMouseDown(event){
+    isPainting = true;
+}
 
-// 벽
-ctx.strokeRect(75, 140, 150, 110);
+function onMouseUp(event){
+    isPainting = false;
+}
 
-// 문
-ctx.fillRect(130, 190, 40, 60);
+function cancleDraw(){
+    isPainting = false;
+    ctx.beginPath();
+}
 
-// 지붕
-ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
+function LineWidth(evnet){
+    ctx.lineWidth = (event.target.value);
+}
+
+function onClickchangeColor(event){
+    ctx.strokeStyle = event.target.dataset.color;
+}
+
+function changeColor(event){
+    colorvalue = event.target.value;
+    ctx.strokeStyle = colorvalue;
+
+}
+
+canvas.addEventListener("mousemove",onMouseMove);
+canvas.addEventListener("mousedown",onMouseDown);
+canvas.addEventListener("mouseup",onMouseUp);
+canvas.addEventListener("mouseleave",cancleDraw);
+
+lineWidth.addEventListener("change",LineWidth);
+color.addEventListener("change",changeColor);
+
+colorOptions.forEach((color) =>
+    color.addEventListener("click",onClickchangeColor));
+
+/* 마우스를 움직일 때 선 그리기
+function drawline(event){
+    ctx.lineTo(event.offsetX,event.offsetY);
+    ctx.strokeStyle = colors[Math.floor(Math.random()*colors.length)];
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove",drawline);*/
+
+
+/* 클릭할 떄 선 그리기
+function drawline(event){
+    ctx.lineTo(event.offsetX,event.offsetY);
+    ctx.strokeStyle = colors[Math.floor(Math.random()*colors.length)];
+    ctx.stroke();
+}
+
+canvas.addEventListener("click",drawline); */
