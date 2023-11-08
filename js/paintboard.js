6c8f52bf82a3b29636873532afa12e45c091a9bf
Seungmin Song <142411910+xngalsdev@users.noreply.github.com>
2023-11-08 09:16:28 +0900

    나의 그림판

--- js/paintboard.js
+++ js/paintboard.js
@@ -1,65 +1,198 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-/* 랜덤 색상
-const colors = [
-    "red","orange","yellow","green","blue","purple","black"
-]*/
 const lineWidth = document.getElementById("line-width");
-const color = document.getElementById("color");
-let isPainting = false;
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const btnMode = document.getElementById("btnMode");
+const btnInit = document.getElementById("btnInit");
+const btnEraser = document.getElementById("btnEraser");
+const inputFile = document.getElementById("file");
+const inputText = document.getElementById("text");
+const btnSave = document.getElementById("save");
 
+let canvasBgColor = "";
 
-canvas.width = 800;
-canvas.height = 800;
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
+//flag 변수
+let isPainting = false;
+let isFilling = false;
+
+
+/**
+ * 캔버스에 선을 그리는 함수
+ */
 function onMouseMove(event){
+
     if (isPainting){
-        ctx.lineTo(event.offsetX,event.offsetY);
+        ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
         return;
     }
-    ctx.moveTo(event.offsetX,event.offsetY);
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
+/**
+ * mouse버튼이 내려갈 때
+ */
 function onMouseDown(event){
     isPainting = true;
 }
 
-function onMouseUp(event){
-    isPainting = false;
-}
-
-function cancleDraw(){
+/**
+ * 그리기를 취소한다.
+ * 선을 그릴 때마다 선 색깔을 바꾼다.
+ */
+function cancelPainting(event){
     isPainting = false;
     ctx.beginPath();
-}
-
-function LineWidth(evnet){
-    ctx.lineWidth = (event.target.value);
-}
-
-function onClickchangeColor(event){
-    ctx.strokeStyle = event.target.dataset.color;
-}
-
-function changeColor(event){
-    colorvalue = event.target.value;
-    ctx.strokeStyle = colorvalue;
 
 }
 
-canvas.addEventListener("mousemove",onMouseMove);
-canvas.addEventListener("mousedown",onMouseDown);
-canvas.addEventListener("mouseup",onMouseUp);
-canvas.addEventListener("mouseleave",cancleDraw);
+/**
+ * 선의 두께를 변경한다.
+ */
+function onChangeLineWidth(event){
+    //console.log(event.target.value);
+    ctx.lineWidth = event.target.value;
+}
 
-lineWidth.addEventListener("change",LineWidth);
-color.addEventListener("change",changeColor);
+/**
+ * 색을 변경한다.
+ */
+function onClickColor(event){
+    //console.dir(event);
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    if (isFilling){
+        canvasBgColor = colorValue;
+    }
+    // console.log(colorValue);
+    // console.log(canvasBgColor);
+}
+
+/**
+ * 모드를 변경한다.
+ * 채우기면 그리기로
+ * 그리기면 채우기로
+ */
+function onMode(){
+    //isFilling true
+    if (isFilling){
+        isFilling = false;
+        btnMode.innerText = "채우기";
+    }else {    //isFilling false
+        isFilling = true;
+        btnMode.innerText = "그리기";
+    }
+}
+
+/**
+ * 캔버스를 선택한 색깔로 채운다
+ */
+function onClickCanvas(event){
+    //console.log("onClickCanvas", event);
+    //isFilling true
+    if (isFilling) {
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    }
+    
+}
+
+/** 
+ * 캔버스를 초기화한다. 
+*/
+function onClickInit(){
+    
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+/**
+ * 그린 그림을 지운다.
+ */
+function onClickEraser(event){
+    //console.dir(event);
+    //ctx.strokeStyle = "white";
+    ctx.strokeStyle = canvasBgColor;
+    isFilling = false;
+    btnMode.innerText = "채우기"
+
+}
+
+/**
+ * 선택한 이미지 파일을 그림판에 추가한다.
+ */
+function onChangeFile(event){
+    console.log(event);
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url ;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        //drawImage(image, dx, dy, dWidth, dHeight)
+        // inputFile.value = null;
+    };
+
+}
+
+/**
+ * 캔버스를 더블클릭했을 때 실행하는 함수
+ * 사용자가 입력한 텍스트를 캔버스에 표시한다.
+ */
+function onDoubleClick(event){
+    // console.dir(event);
+    const text = inputText.value;
+    if (text !== ""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "60px 'consolas'";
+        ctx.strokeText(text, event.offsetX, event.offsetY);
+        // ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+
+/**
+ * 작업한 이미지를 파일로 저장한다.
+ */
+function saveImage(){
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click();
+}
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onClickCanvas);
+lineWidth.addEventListener("change", onChangeLineWidth);
 
 colorOptions.forEach((color) =>
-    color.addEventListener("click",onClickchangeColor));
+    color.addEventListener("click", onClickColor));
+
+btnMode.addEventListener("click", onMode);
+
+btnInit.addEventListener("click", onClickInit);
+
+btnEraser.addEventListener("click", onClickEraser);
+
+inputFile.addEventListener("change", onChangeFile);
+canvas.addEventListener("dblclick", onDoubleClick);
+
+btnSave.addEventListener("click", saveImage);
+
+
 
 /* 마우스를 움직일 때 선 그리기
 function drawline(event){
